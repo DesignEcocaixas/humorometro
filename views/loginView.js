1db50999c1e070a988e59c79e5104f139aa0ff4f
DesignEcocaixas <designecocaixasba@gmail.com>
2026-01-28 12:04:51 -0300

    Update loginView.js

--- views/loginView.js
+++ views/loginView.js
@@ -6,6 +6,8 @@ function loginView() {
     <meta charset="UTF-8">
     <title>Login - Administração</title>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* ===== LOADING GLOBAL ===== */
